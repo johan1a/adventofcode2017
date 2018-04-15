03e6da0a5d0288fcb3c5beb2b0b8f920575beb5c
Johan Andersson <joha@netcompany.com>
2018-04-15 08:45:21 +0200

    day1 part 2

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -23,3 +23,27 @@
   []
   (let [input (read-input)]
       (calc-scores input 0)))
+
+(defn get-other
+  [input index]
+  (let [size (count input)
+        offset (mod (+ (/ size 2) index) size)]
+    (nth input offset)))
+
+(defn calc-scores2
+  [input index acc]
+  (if (= index (count input)) acc
+      (let [a (nth input index)
+            b (get-other input index)
+            score (calc-score a b)]
+        (recur input (inc index) (+ acc score)))))
+
+(defn read-input2
+  []
+  (let [input (str/trim-newline (slurp "input.txt"))]
+  (map #(Integer/parseInt (str %)) input)))
+
+(defn part2
+  []
+  (let [input (read-input2)]
+      (calc-scores2 input 0 0)))
